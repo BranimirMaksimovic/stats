ffb6675753d9e5f5b21b1ab433f09ecae9dcd56f
Branimir Maksimovic <maxava84@gmail.com>
2022-06-22 12:06:29 +0200

    unpack-csv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+let maUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    maUnitedWins++;
+  } else if (match[2] == 'Man United' && match[5] === 'A') {
+    maUnitedWins++;
+  }
+}
+console.log(`Man United won ${maUnitedWins} games`);
